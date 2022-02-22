d07715c458587c8094aea38bcc639c8a23dc4abc
Mayil Kerimov <mayilik@code.edu.az>
2022-02-22 13:12:52 +0400

    first commiy

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function Open(){
+    var sidbar = document.getElementById('sidebar');
+    sidbar.style.transform = 'translateX(0%)';
+}
+
+function Close(){
+    var sidbar = document.getElementById('sidebar');
+    sidbar.style.transform = 'translateX(100%)'
+}
